1a9483dd0b27ef23f58cd7d0aa28dfee5c33a501
Angela Potapchuk <angela.harkov@gmail.com>
2023-05-19 00:19:52 +0300

    Created App structure

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,35 @@
 import { Component } from 'react';
+import { Section } from './Section/Section';
+import { Header } from './Header/Header';
+import { ContactForm } from './ContactForm/ContactForm';
+import { ContactList } from './ContactList/ContactList';
 import { GlobalStyle } from './GlobalStyle';
 
 export default class App extends Component {
+  state = {
+    contacts: [],
+    name: ''
+  }
+
+  getVisibleContacts = () => {
+    const { filter, contacts } = this.state;
+    const normalizedFilter = filter.toLowerCase();
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+  };
+
   render() {
+    const visibleContacts = this.getVisibleContacts();
     return (
       <>
-        <div>App</div>
+        <Section title="Phonebook">
+          <ContactForm/>
+          <Header title="Contacts" />
+          <ContactList
+            contacts={visibleContacts}
+          />
+        </Section>
         <GlobalStyle />
       </>
     );
