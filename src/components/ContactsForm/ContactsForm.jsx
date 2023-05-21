9e43fbfecd139fe5db525c02041c95392de7202a
Angela Potapchuk <angela.harkov@gmail.com>
2023-05-21 15:21:26 +0300

    Install legacy-peer-deps

--- src/components/ContactsForm/ContactsForm.jsx
+++ src/components/ContactsForm/ContactsForm.jsx
@@ -68,7 +68,7 @@ class ContactsForm extends Component {
               placeholder='name'
               onChange={this.onInputChange}
               value={name}
-              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+              pattern="/^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$/"
               title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
               required
             />
@@ -82,7 +82,7 @@ class ContactsForm extends Component {
               placeholder='tel number'
               onChange={this.formatNumber}
               value={number}
-              pattern='\+?\d{1,4}[-.\s]?\d{1,3}[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}'
+              pattern="\+?\d{1,4}[-.\s]?\d{1,3}[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}/"
               title='Phone number must be digits and can contain spaces, dashes, parentheses and can start with +'
               required
             />
